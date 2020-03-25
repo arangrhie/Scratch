e7bfb251d54a6cca6be2b4d25c26266cae828f95
arangrhie <arrhie@gmail.com>
2020-03-25 01:43:13 -0400

    scale per hapmer (X) and plot the size (Y) with optional log transformation (Y)

--- Merqury/run.R
+++ Merqury/run.R
@@ -7,4 +7,15 @@
 setwd("./Merqury/")
 source("./src/plot_blob.R")
 
-blob_plot(dat = "input/canu.hapmers.count", out = "output/canu")
+blob_plot(dat = "input/triocanu_clr.hapmers.count", out = "output/triocanu", type = "scale")
+blob_plot(dat = "input/canu.hapmers.count", out = "output/canu", type = "scale")
+blob_plot(dat = "input/falcon_unzip.hapmers.count", out = "output/falcon_unzip", type = "scale")
+
+blob_plot(dat = "input/triocanu_clr.hapmers.count", type = "scale", plot_log = TRUE, out = "output/triocanu.log")
+blob_plot(dat = "input/canu.hapmers.count", type = "scale", plot_log = TRUE, out = "output/canu.log")
+blob_plot(dat = "input/falcon_unzip.hapmers.count", type = "scale", plot_log = TRUE, out = "output/falcon_unzip.log")
+
+dat = "input/canu.hapmers.count"
+dat=read.table(dat, header=TRUE)
+plot_blob_exact(dat)
+plot_blob_scaled(dat)
--- Merqury/src/plot_blob.R
+++ Merqury/src/plot_blob.R
@@ -7,23 +7,22 @@ require("scales")
 parser <- ArgumentParser(description = "Make blob plots.")
 parser$add_argument("-f", "--file", type="character", help=".count tdf; with headers; ie. <category> <seqId> <hap1Count> <hap2Count> <seqSize> (required)", default=NULL)
 parser$add_argument("-o", "--output", type="character", default="hapmers.blob", help="output prefix [default %(default)s]")
+parser$add_argument("-t", "--type", type="character", default="exact", help="available types: exact or scaled. [default %(default)s]" )
 parser$add_argument("-x", "--xdim", type="double", default=6.5, help="width of output plot [default %(default)s]")
 parser$add_argument("-y", "--ydim", type="double", default=6, help="height of output plot [default %(default)s]")
 parser$add_argument("-p", "--pdf", dest='pdf', default=FALSE, action='store_true', help="set to get output in .pdf. [default .png]")
 args <- parser$parse_args()
 
-blob_plot <- function(dat=NULL, out, w=6.5, h=6, pdf=FALSE) {
-
-  dat=read.table(dat, header=TRUE)
-
+plot_blob_exact <- function(dat=NULL) {
+  
   max_total=max(max(dat[,3]), max(dat[,4])) * 1.01
   col_lab=names(dat)[1]
   x_lab=names(dat)[3]
   y_lab=names(dat)[4]
   print(x_lab)
   print(y_lab)
-
-  ggplot(dat, aes(x=dat[,3], y=dat[,4], fill=dat[,1], size=dat[,5])) +
+  
+  p <- ggplot(dat, aes(x=dat[,3], y=dat[,4], fill=dat[,1], size=dat[,5])) +
     geom_point(shape=21, alpha=0.3) + theme_bw() +
     scale_color_brewer(palette = "Set1") +
     scale_fill_brewer(palette = "Set1") +     # Set1 -> Red / Blue. Set2 -> Green / Orange.
@@ -40,7 +39,68 @@ blob_plot <- function(dat=NULL, out, w=6.5, h=6, pdf=FALSE) {
     guides( size = guide_legend(order = 1),
             fill = guide_legend(override.aes = list(size=5, alpha=1), order = 2, title = col_lab)) +
     xlab(x_lab) + ylab(y_lab)
+  
+  p
+  return(p)
+}
 
+plot_blob_scaled <- function(dat=NULL, plot_log=FALSE) {
+  head(dat)
+  dat$Scld=ifelse(dat[,3] == dat[,4], 0.5, 100*dat[,4]/(dat[,3]+dat[,4]))
+  
+  max_total=max(dat[,5]) * 1.01
+  col_lab=names(dat)[1]
+  x_lab=paste(names(dat)[4] , "(%)")
+  y_lab=names(dat)[5]
+  print(max_total)
+  print(x_lab)
+  print(y_lab)
+  
+  p <- ggplot(dat, aes(x=Scld, y=Size, fill=dat[,1], size=dat[,5])) +
+    geom_point(shape=21, alpha=0.3) + theme_bw() +
+    scale_color_brewer(palette = "Set1") +
+    scale_fill_brewer(palette = "Set1")
+
+  if (!plot_log) {
+    # Plot in standard continuous Y
+    p <- p + 
+      scale_y_continuous(labels=comma, limits = c(0, max_total))
+  } else {
+    # Plot in log scaled Y
+    p <- p + 
+      scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x)), limits = c(1, max_total)) +
+      annotation_logticks(sides = "lr")
+  }
+  p <- p +
+    scale_size_continuous(labels=comma, range = c(1, 10), name = "Total k-mers (size)") +
+    theme(legend.text = element_text(size=11),
+          legend.background = element_rect(size=0.5, linetype="solid", colour ="black"),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=12)) +
+    guides( size = guide_legend(order = 1),
+            fill = guide_legend(override.aes = list(size=5, alpha=1), order = 2, title = col_lab)) +
+    xlab(x_lab) + ylab(y_lab)
+  
+  return(p)
+}
+
+
+
+blob_plot <- function(dat=NULL, out, w=6.5, h=6, pdf=FALSE, type="exact", plot_log=FALSE) {
+
+  dat=read.table(dat, header=TRUE)
+  
+  if (type == "exact") {
+    print("Plot exact k-mer counts")
+    p <- plot_blob_exact(dat)
+  } else {
+    print("plot scaled k-mer counts")
+    w=8
+    p <- plot_blob_scaled(dat, plot_log)
+  }
+  p
+  
   outformat="png"
   if (pdf) {
     outformat="pdf"
@@ -49,5 +109,5 @@ blob_plot <- function(dat=NULL, out, w=6.5, h=6, pdf=FALSE) {
 
 }
 
-blob_plot(dat = args$file, out = args$output, w = args$xdim, h = args$ydim, pdf = args$pdf)
+blob_plot(dat = args$file, out = args$output, w = args$xdim, h = args$ydim, pdf = args$pdf, type=args$type, plot_log = args$plot_log)
 
