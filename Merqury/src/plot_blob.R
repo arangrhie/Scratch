4ea9ddad82c0f075049ae8a7f1fa959138b0d7b3
arangrhie <arrhie@gmail.com>
2020-03-31 12:08:54 -0400

    add ymax

--- Merqury/src/plot_blob.R
+++ Merqury/src/plot_blob.R
@@ -8,14 +8,30 @@ parser <- ArgumentParser(description = "Make blob plots.")
 parser$add_argument("-f", "--file", type="character", help=".count tdf; with headers; ie. <category> <seqId> <hap1Count> <hap2Count> <seqSize> (required)", default=NULL)
 parser$add_argument("-o", "--output", type="character", default="hapmers.blob", help="output prefix [default %(default)s]")
 parser$add_argument("-t", "--type", type="character", default="exact", help="available types: exact or scaled. [default %(default)s]" )
+parser$add_argument("-m", "--ymax", type="double", default=0, help="maximum Y scale, auto detect max if 0 [default %(default)s]")
 parser$add_argument("-x", "--xdim", type="double", default=6.5, help="width of output plot [default %(default)s]")
 parser$add_argument("-y", "--ydim", type="double", default=6, help="height of output plot [default %(default)s]")
 parser$add_argument("-p", "--pdf", dest='pdf', default=FALSE, action='store_true', help="set to get output in .pdf. [default .png]")
 args <- parser$parse_args()
 
-plot_blob_exact <- function(dat=NULL) {
+fancy_scientific <- function(d) {
+  # turn in to character string in scientific notation
+  d <- format(d, scientific = TRUE)
+  # quote the part before the exponent to keep all the digits and turn the 'e+' into 10^ format
+  d <- gsub("^(.*)e\\+", "'\\1'%*%10^", d)
+  # convert 0x10^00 to 0
+  d <- gsub("\\'0[\\.0]*\\'(.*)", "'0'", d)
+  # return this as an expression
+  parse(text=d)
+}
+
+plot_blob_exact <- function(dat=NULL, ymax) {
   
-  max_total=max(max(dat[,3]), max(dat[,4])) * 1.01
+  if (ymax == 0) {
+    max_total = max(max(dat[,3]), max(dat[,4])) * 1.1
+  } else {
+    max_total = ymax
+  }
   col_lab=names(dat)[1]
   x_lab=names(dat)[3]
   y_lab=names(dat)[4]
@@ -26,16 +42,16 @@ plot_blob_exact <- function(dat=NULL) {
     geom_point(shape=21, alpha=0.3) + theme_bw() +
     scale_color_brewer(palette = "Set1") +
     scale_fill_brewer(palette = "Set1") +     # Set1 -> Red / Blue. Set2 -> Green / Orange.
-    scale_x_continuous(labels=comma, limits = c(0, max_total)) +
-    scale_y_continuous(labels=comma, limits = c(0, max_total)) +
-    scale_size_continuous(labels=comma, range = c(1, 10), name = "Total k-mers (size)") +
-    theme(legend.text = element_text(size=11),
-          legend.position = c(0.95,0.95),  # Modify this if the legend is covering your favorite circle
+    scale_x_continuous(labels = comma, limits = c(0, max_total)) +
+    scale_y_continuous(labels = comma, limits = c(0, max_total)) +
+    scale_size_continuous(labels = comma, range = c(1, 10), name = "Total k-mers (size)") +
+    theme(legend.text = element_text(size=12),
+          #legend.position = c(0.95,0.95),  # Modify this if the legend is covering your favorite circle
           legend.background = element_rect(size=0.5, linetype="solid", colour ="black"),
-          legend.box.just = "right",
-          legend.justification = c("right", "top"),
+          #legend.box.just = "right",
+          #legend.justification = c("right", "top"),
           axis.title=element_text(size=14,face="bold"),
-          axis.text=element_text(size=12)) +
+          axis.text=element_text(size=11)) +
     guides( size = guide_legend(order = 1),
             fill = guide_legend(override.aes = list(size=5, alpha=1), order = 2, title = col_lab)) +
     xlab(x_lab) + ylab(y_lab)
@@ -44,11 +60,15 @@ plot_blob_exact <- function(dat=NULL) {
   return(p)
 }
 
-plot_blob_scaled <- function(dat=NULL, plot_log=FALSE) {
+plot_blob_scaled <- function(dat=NULL, ymax = 0, plot_log=FALSE) {
   head(dat)
   dat$Scld=ifelse(dat[,3] == dat[,4], 0.5, 100*dat[,4]/(dat[,3]+dat[,4]))
   
-  max_total=max(dat[,5]) * 1.01
+  if (ymax == 0) {
+    max_total = max(dat[,5]) * 1.1
+  } else {
+    max_total = ymax
+  }
   col_lab=names(dat)[1]
   x_lab=paste(names(dat)[4] , "(%)")
   y_lab=names(dat)[5]
@@ -87,17 +107,17 @@ plot_blob_scaled <- function(dat=NULL, plot_log=FALSE) {
 
 
 
-blob_plot <- function(dat=NULL, out, w=6.5, h=6, pdf=FALSE, type="exact", plot_log=FALSE) {
+blob_plot <- function(dat=NULL, out, ymax = 0, w=6.5, h=6, pdf=FALSE, type="exact", plot_log=FALSE) {
 
   dat=read.table(dat, header=TRUE)
   
   if (type == "exact") {
     print("Plot exact k-mer counts")
-    p <- plot_blob_exact(dat)
+    p <- plot_blob_exact(dat, ymax)
   } else {
     print("plot scaled k-mer counts")
     w=8
-    p <- plot_blob_scaled(dat, plot_log)
+    p <- plot_blob_scaled(dat, ymax, plot_log)
   }
   p
   
@@ -109,5 +129,5 @@ blob_plot <- function(dat=NULL, out, w=6.5, h=6, pdf=FALSE, type="exact", plot_l
 
 }
 
-blob_plot(dat = args$file, out = args$output, w = args$xdim, h = args$ydim, pdf = args$pdf, type=args$type, plot_log = args$plot_log)
+blob_plot(dat = args$file, out = args$output, ymax = args$ymax, w = args$xdim, h = args$ydim, pdf = args$pdf, type=args$type, plot_log = args$plot_log)
 
